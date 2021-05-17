00a8be1e2a329ea6f2fa63f8c718b23a84cc43ab
Efereyan Karen Simisola <thedevkaren@gmail.com>
2021-05-17 20:36:59 +0100

    Finished mobile iteration

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 let acc = document.getElementsByClassName("accordion");
 let spanBtns = document.getElementsByClassName("faq-btn"); 
-let i, j, k;
-
-//console.log("The featured items are:", featuredItem);
-//console.log("The span buttons are:", spanBtns);
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-   // console.log("The value of spanBtns is", spanBtns);
+
     
     if (panel.style.display === "block") {
-      spanBtns[0].innerText = "+";
       panel.style.display = "none";
     } else {
-      spanBtns[0].innerText ="x";
       panel.style.display = "block";
     }
   
